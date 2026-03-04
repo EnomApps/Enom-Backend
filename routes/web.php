345297eb4156df1b95ad65a789486298e7aeeab6
veeraiyan <veeraiyan1991@gmail.com>
2026-03-05 01:47:01 +0530

    image_preview

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile-images/{filename}', function (string $filename) {
+    if (str_contains($filename, '/') || str_contains($filename, '..')) {
+        abort(404);
+    }
+    $path = public_path('profile-images/' . $filename);
+    if (!file_exists($path) || !is_file($path)) {
+        abort(404);
+    }
+    return response()->file($path);
+});
